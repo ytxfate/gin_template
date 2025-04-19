aa55cc9af131c0bbae1b6e5c703b7652e7ab5581
ytx <1426482738@qq.com>
2025-04-19 19:30:49 +0800

    🎈 perf(logger): stdout及文件记录

--- project/utils/logger/logger.go
+++ project/utils/logger/logger.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"os"
+	"time"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -10,17 +11,26 @@ import (
 var Logger *zap.Logger
 
 func InitLogger() {
-	Logger = zap.New(zapcore.NewCore(
-		zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig()),
-		// gopkg.in/natefinch/lumberjack.v2
-		// zapcore.AddSync(&lumberjack.Logger{
-		// 	Filename:   "./test.log",
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = func(t time.Time, pae zapcore.PrimitiveArrayEncoder) {
+		pae.AppendString(t.Format("2006-01-02 15:04:05.000"))
+	}
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	encoderConfig.EncodeCaller = zapcore.FullCallerEncoder
+	consoleEncoder := zapcore.NewConsoleEncoder(encoderConfig)
+	// fileEncoder := zapcore.NewConsoleEncoder(encoderConfig)
+	core := zapcore.NewTee(
+		// 标准输出日志
+		zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), zapcore.DebugLevel),
+		// 文件记录日志  gopkg.in/natefinch/lumberjack.v2
+		// zapcore.NewCore(fileEncoder, zapcore.AddSync(&lumberjack.Logger{
+		// 	Filename:   "logs/api.log",
 		// 	MaxSize:    1,
-		// 	MaxBackups: 5,
-		// 	MaxAge:     30,
+		// 	MaxBackups: 2,
+		// 	MaxAge:     10,
 		// 	Compress:   true,
-		// }),
-		zapcore.AddSync(os.Stdout),
-		zapcore.DebugLevel,
-	))
+		// }), zapcore.DebugLevel),
+	)
+	Logger = zap.New(core)
+
 }
