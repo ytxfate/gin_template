e9861a053834ac66412efc79b52374981eda4307
ytx <1426482738@qq.com>
2025-12-08 22:02:09 +0800

    🌈 style(model):

--- internal/api/repository/user/user.go
+++ internal/api/repository/user/user.go
@@ -1,6 +1,6 @@
 package user
 
-import "gin_template/pkg/models"
+import "gin_template/internal/api/models"
 
 func FindUserByUserPwd(username, password string) (*models.User, error) {
 	// TODO: db query
