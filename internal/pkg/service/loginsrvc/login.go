8103cbf9f7a158527474140123f276c915197792
ytx <1426482738@qq.com>
2026-01-09 22:48:42 +0800

    🦄 refactor(cust_err): naming optimization

--- internal/pkg/service/loginsrvc/login.go
+++ internal/pkg/service/loginsrvc/login.go
@@ -5,17 +5,17 @@ import (
 	"gin_template/internal/backend/jwttool"
 	"gin_template/internal/backend/webconfig"
 	"gin_template/internal/pkg/commresp"
-	"gin_template/internal/pkg/customerrors"
+	"gin_template/internal/pkg/custerr"
 	"gin_template/internal/pkg/repository/user"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-func Login(username, password string) (tk string, refreshTk string, custErr *customerrors.CustErr) {
+func Login(username, password string) (tk string, refreshTk string, custErr *custerr.CustErr) {
 	// TODO: 用户信息验证
 	_, err := user.FindUserByUserPwd(username, password)
 	if err != nil {
-		custErr = &customerrors.CustErr{
+		custErr = &custerr.CustErr{
 			Code: commresp.JwtCreateError,
 			Err:  errors.New("用户名密码错误"),
 		}
@@ -23,7 +23,7 @@ func Login(username, password string) (tk string, refreshTk string, custErr *cus
 	}
 	tk, refreshTk, err = jwttool.GenerateJWTAndRefreshJWT(&jwttool.JWTInfo{UserName: username}, webconfig.Cfg.Web.SecretKey)
 	if err != nil {
-		custErr = &customerrors.CustErr{
+		custErr = &custerr.CustErr{
 			Code: commresp.JwtCreateError,
 			Err:  errors.New("JWT 信息生成异常"),
 		}
@@ -31,10 +31,10 @@ func Login(username, password string) (tk string, refreshTk string, custErr *cus
 	return
 }
 
-func RefreshToken(tk, refreshTk string) (newTk string, newRefreshTk string, custErr *customerrors.CustErr) {
+func RefreshToken(tk, refreshTk string) (newTk string, newRefreshTk string, custErr *custerr.CustErr) {
 	_, jwtInfo, err := jwttool.ValidateJWT(tk, webconfig.Cfg.Web.SecretKey, jwt.WithoutClaimsValidation()) // 获取 jwt 存储信息, 不校验是否失效
 	if err != nil {
-		custErr = &customerrors.CustErr{
+		custErr = &custerr.CustErr{
 			Code: commresp.UserLogout,
 			Err:  errors.New("登录失效"),
 		}
@@ -42,7 +42,7 @@ func RefreshToken(tk, refreshTk string) (newTk string, newRefreshTk string, cust
 	}
 	stat := jwttool.ValidateRefreshJWT(tk, refreshTk, webconfig.Cfg.Web.SecretKey)
 	if !stat {
-		custErr = &customerrors.CustErr{
+		custErr = &custerr.CustErr{
 			Code: commresp.UserNoLogin,
 			Err:  errors.New("refresh token 验证失败"),
 		}
@@ -50,7 +50,7 @@ func RefreshToken(tk, refreshTk string) (newTk string, newRefreshTk string, cust
 	}
 	newTk, newRefreshTk, err = jwttool.GenerateJWTAndRefreshJWT(&jwtInfo, webconfig.Cfg.Web.SecretKey)
 	if err != nil {
-		custErr = &customerrors.CustErr{
+		custErr = &custerr.CustErr{
 			Code: commresp.JwtCreateError,
 			Err:  errors.New("JWT 信息生成异常"),
 		}
